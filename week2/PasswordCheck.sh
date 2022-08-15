0ac47f001ce86640162079da506f7c5b864a447f
Jacinta Hayward <91399310+SintiHaze@users.noreply.github.com>
2022-08-15 19:55:18 +0800

    getting rid of the errors and adding variable

--- week2/PasswordCheck.sh
+++ week2/PasswordCheck.sh
@@ -11,11 +11,16 @@ echo "Type a folder name"
 read -p "Type the name of the folder you would like to create: " folderName
 
 # ask the user to type a secret password (hide that input)
-read -sp "Type your password: "
-
-# save the user’s password in a file called 'secret.txt' inside the folder the user chose.
+read -sp "Type your password: " 
 
 # Turn the password into a hash
-echo SECRET_PASSWORD | sha256sum
-echo SECRET_PASSWORD >> secret.txt
+echo 
+echo $password | sha256sum
+
+# save the user’s password in a file called 'secret.txt' 
+echo password >> secret.txt
+
+# Moves the secret.txt file to inside the folder the user chose.
+mv secret.txt folerName
+
 echo "This is a hash of your password."
