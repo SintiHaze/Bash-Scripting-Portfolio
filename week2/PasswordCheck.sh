81063fb7697eeb4ab30c7fff625e2252a3018a4e
Jacinta Hayward <91399310+SintiHaze@users.noreply.github.com>
2022-08-15 20:22:29 +0800

    Got rid of the issues. Added ability to create a folder using folderName variable. Works effectively now.

--- week2/PasswordCheck.sh
+++ week2/PasswordCheck.sh
@@ -7,20 +7,21 @@
 # Date modified: 14/08/2022
 
 # ask the user to type a folder name
-echo "Type a folder name"
 read -p "Type the name of the folder you would like to create: " folderName
 
-# ask the user to type a secret password (hide that input)
-read -sp "Type your password: " 
+# Creates a folder with the name provided by the user
+mkdir -p $folderName
 
-# Turn the password into a hash
+# ask the user to type a secret password (hide that input)
+read -sp "Type your password: " pass_var
 echo 
-echo $password | sha256sum
+# Turn the password into a hash
+echo $pass_var | sha256sum
+echo $pass_var | sha256sum > secret.txt
 
 # save the user’s password in a file called 'secret.txt' 
-echo password >> secret.txt
+echo
+echo "This is a hash of your password."
 
 # Moves the secret.txt file to inside the folder the user chose.
-mv secret.txt folerName
-
-echo "This is a hash of your password."
+mv secret.txt $folderName
