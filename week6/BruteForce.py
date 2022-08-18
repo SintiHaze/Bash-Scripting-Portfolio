dbf08622403e9ac08b37a0d25261437e91ed051a
Jacinta Hayward <91399310+SintiHaze@users.noreply.github.com>
2022-08-18 20:44:52 +0800

    Brutforece.py

--- week6/BruteForce.py
+++ week6/BruteForce.py
@@ -4,6 +4,28 @@
 
 # Author: Jacinta Hayward
 # Date Created: 14/08/2022
-# Date modified: 16/08/2022
+# Date modified: 17/08/2022
 
+# Imports tha random function
+import random
 
+# Sets the different characters that can be used in the inputted password
+character = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+characterList = list(character)
+
+# Asks the user to input a password
+password = input("\033[1;32m Please enter your password: ")
+guess = ""
+while (guess != password):
+    guess = random.choices(characterList,k=len(password))
+    guess = "".join(guess) 
+print("\033[1;32m Your password is: " + guess)
+
+# Displays an ASCII image
+print("""\
+ ___________________________________ 
+|  _______________________________  |
+| | PASSWORD SUCCESSFULLY CRACKED | |
+| |_______________________________| |
+|___________________________________|
+    """)
