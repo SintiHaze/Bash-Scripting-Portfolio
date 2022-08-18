abc83c22177d3697f0627637a799954c1e0bd537
Jacinta Hayward <91399310+SintiHaze@users.noreply.github.com>
2022-08-18 20:50:44 +0800

    Additional comments added. Expanded the amount of characters that can be used for the password

--- week6/BruteForce.py
+++ week6/BruteForce.py
@@ -9,17 +9,16 @@
 # Imports tha random function
 import random
 
-# Sets the different characters that can be used in the inputted password
-character = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+# Sets the different characters that can be used in the inputted password. Includes letters, numbers and special characters which take makes the password longer to crack.
+character = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()_-+=[{]}|:;'\",<.>/? "
 characterList = list(character)
 
 # Asks the user to input a password
 password = input("\033[1;32m Please enter your password: ")
 guess = ""
 while (guess != password):
-    guess = random.choices(characterList,k=len(password))
-    guess = "".join(guess) 
-print("\033[1;32m Your password is: " + guess)
+    guess = random.choices(characterList,k=len(password)) # The random function allows the system to generate random characters as a way to guess the password.
+    guess = "".join(guess)
 
 # Displays an ASCII image
 print("""\
@@ -29,3 +28,6 @@ print("""\
 | |_______________________________| |
 |___________________________________|
     """)
+
+# Displays the password that was cracked by this code. 
+print("\033[1;32m Your password is: " + guess)
