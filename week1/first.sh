b910995f1b051df8109f4e784fc29cd9e05888ef
Jacinta Hayward <91399310+SintiHaze@users.noreply.github.com>
2022-08-18 22:08:34 +0800

    Week 1 Project. Added comments to describe bash commands.

--- week1/first.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "I hope you are having a beautiful day."
-exit 0
---
+++ week1/second.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Second
+
+# Author: Jacinta Hayward
+# Date Created: 14/07/2022
+# Date modified: 16/08/2022
+
+# Prints the greeting
+echo "Hi there!"
+
+# Prints the greeting but with the input from the command line. 
+echo "It's good to see you $1!"
+
+# Command to exit the bash script.
+exit 0
+					
