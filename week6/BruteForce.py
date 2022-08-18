c6520a5755058fd9f8ecc559641c7010e253040e
Jacinta Hayward <91399310+SintiHaze@users.noreply.github.com>
2022-08-18 21:18:17 +0800

    Added the time function/variable to count the amount of seconds it takes to crack the password with bruteforce.

--- week6/BruteForce.py
+++ week6/BruteForce.py
@@ -5,8 +5,9 @@
 # Date Created: 14/08/2022
 # Date modified: 17/08/2022
 
-# Imports tha random function
+# Imports the random and time functions
 import random
+import time
 
 # Sets the different characters that can be used in the inputted password. Includes letters, numbers and special characters which take makes the password longer to crack.
 character = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()_-+=[{]}|:;'\",<.>/? "
@@ -15,11 +16,14 @@ characterList = list(character)
 # Asks the user to input a password
 password = input("\033[1;32m Please enter your password: ")
 
-# Brute Force function
+# Uses the time function to count the seconds elapsed whilst the password guesses are generated. 
+startTime = time.time() 
 guess = ""
 while (guess != password):
     guess = random.choices(characterList, k=len(password))  # The random function allows the system to generate random characters to guess the password.
     guess = "". join(guess)
+    endTime = time.time()
+    elapsedTime = endTime - startTime
 
 # Displays an ASCII image
 print("""\
@@ -32,3 +36,4 @@ print("""\
 
 # Displays the password that was cracked by this code.
 print("\033[1;32m Your password is: " + guess)
+print("That took", elapsedTime, "seconds to crack!")
