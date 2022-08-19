650283bfe41b5998ae110929749ab702d29f456e
Jacinta Hayward <91399310+SintiHaze@users.noreply.github.com>
2022-08-19 11:15:17 +0800

    Added ability to save the score (amount of guesses) to a text file and then print the scores.

--- week4/GuessingGame.sh
+++ week4/GuessingGame.sh
@@ -6,7 +6,7 @@
 
 # Introduction text and instructions
 echo "Welcome to the Guessing Game!"
-echo "Please type a number between 1 and 100."
+echo -e "\nPlease type a number between 1 and 100."
 
 # Defining the variables
 answer=42              # The chosen number that the player must guess
@@ -24,4 +24,13 @@ while [ $guess -ne $answer ]; do            # If the guess is not equal to the a
     num_guess=$(($num_guess + 1))
 done
 echo -e "\033[32m CORRECT! You guessed $answer in $num_guess guesses."
+
+# Save your score to a text file. 
+read -p "Please enter your name: " name
+echo $name $num_guess >> scores.txt
+
+# Prints list of scores that are saved in the text file. 
+echo -e "\nPrevious scores:" 
+cat scores.txt
+
 exit 0
