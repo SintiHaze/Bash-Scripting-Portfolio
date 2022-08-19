77cc3833772a2ae93e4305357667629d354c94bc
Jacinta Hayward <91399310+SintiHaze@users.noreply.github.com>
2022-08-19 11:05:05 +0800

    Added comments and function to count the amount of guesses the player has made.

--- week4/GuessingGame.sh
+++ week4/GuessingGame.sh
@@ -1,62 +1,27 @@
 #!/bin/bash
 
-# Guessing Game
-
 # Author: Jacinta Hayward
-# Date Created: 14/08/2022
-# Date modified: 16/08/2022
-
-#This function prints when the answer is correct
-
-printCorrect()
-
-{
-
-    echo -e "\033[32mCORRECT! \033[0m $1"
-
-}
-
-# This function prints when the answer is incorrect.
-
-printError()
-
-{
-
-    echo -e "\033[31m Too Low! \033[0m $1"
-
-}
-
-
-
-# This function will get a value between the 2nd and 3rd arguments
-
-getNumber()
-
-{
-
-    read -p "$1: "
-
-    while (( $REPLY > $2 || $REPLY < $3 )); do
-
-        printCorrect
-
-        read -p "$1: "
-
-    done
-
-    while (( $REPLY < $2 || $REPLY > $3 )); do
-
-        printError "Input must be between $2 and $3"
-
-        read -p "$1: "
-
-    done
-
-}
+# Date Created: 19/08/2022
+# Date modified: 19/08/2022
 
 # Introduction text and instructions
-
 echo "Welcome to the Guessing Game!"
+echo "Please type a number between 1 and 100."
 
-getNumber "Please type a number between 1 and 100" 1 100
+# Defining the variables
+answer=42              # The chosen number that the player must guess
+guess=0                # The number that is guessed by the player
+num_guess=0            # Number of guesses made by the player
 
+# Play game
+while [ $guess -ne $answer ]; do            # If the guess is not equal to the answer variable, it will prompt the player to make a guess.
+    echo -n "What is the number? " ; read guess
+    if (( guess < answer )) ; then          # If the players guess is less than than the answer variable, then it will print "Higher"
+    echo "...Higher!"
+    elif (( guess > answer )) ; then        # If the players guess is greater than the answer variable, then it will print "Lower"
+    echo "...Lower!"
+    fi
+    num_guess=$(($num_guess + 1))
+done
+echo -e "\033[32m CORRECT! You guessed $answer in $num_guess guesses."
+exit 0
